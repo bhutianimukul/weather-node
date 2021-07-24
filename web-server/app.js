70f1d2c6b05794ae8c2b0952f960eb1a83f48825
bhutianimukul <bhutianimukul@gmail.com>
2021-07-24 19:03:45 +0530

    first commit

---
+++ web-server/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const request = require("request");
+
+const getWeather = function (city, callback) {
+  const url =
+    "http://api.weatherstack.com/current?access_key=d22b3fcd47c57627984eb86f942e9458&query=" +
+    city;
+  request({ url: url, json: true }, (err, response) => {
+    if (err) callback("unable to found", undefined, undefined);
+    else if (response.body.error)
+      callback("Unable to find", undefined, undefined);
+    //console.log(response);
+    else {
+      const data = response.body;
+      let temp = data.current.temperature;
+      let feelsLike = data.current.feelslike;
+      callback(undefined, temp, feelsLike);
+    }
+  });
+};
+
+//app.com/help
+//app.com/about
+//app.com/
+app.set("view engine", "hbs");
+const path = __dirname + "/views/partials";
+hbs.registerPartials(path);
+app.use(express.static(__dirname + "/public"));
+app.get("", (req, res) => {
+  // res.sendFile(__dirname + `/public/index.html`);
+  res.render("index");
+});
+app.get("/help", (req, res) => {
+  //res.sendFile(__dirname + "/public/help.html");
+  res.render("help");
+});
+
+app.get("/about", (req, res) => {
+  //res.sendFile(__dirname + `/public/about.html`);
+  res.render("about", { title: "about me", name: "mukul" });
+});
+app.get("/weather", (req, res) => {
+  if (!req.query.search) res.send({ error: "Unable to search" });
+  else {
+    getWeather(req.query.search, (err, temp, feelsLike) => {
+      if (err) res.send({ error: "unable to find location" });
+      else {
+        res.send({
+          forecast: temp,
+          feelsLike: feelsLike,
+        });
+      }
+    });
+    //res.send("hi");
+  }
+});
+app.get("*", (req, res) => res.render("404"));
+app.listen(3000, () => console.log("server started"));
---
+++ web-server/public/js/script.js
@@ -0,0 +1,30 @@
+// console.log("hello");
+const msg1 = document.getElementById("temp");
+const msg2 = document.getElementById("feelsLike");
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  document.querySelector("input").value.textContent = "";
+  e.preventDefault();
+  msg2.textContent = "";
+  msg1.textContent = "";
+
+  const location = document.querySelector("input").value;
+  fetchWeather(location);
+});
+const fetchWeather = function (location) {
+  msg1.textContent = "........Loading";
+  msg2.textContent = "";
+  const url = `http://localhost:3000/weather?search=${location}`;
+  fetch(url)
+    .then((res) => {
+      res.json().then((response) => {
+        if (response.error) msg1.textContent = response.error;
+        else {
+          msg1.textContent = "Today's weather is " + response.forecast + "°C";
+          msg2.textContent = "It feels Like " + response.feelsLike + "°C";
+        }
+      });
+    })
+    .catch((err) => console.log("error"));
+};
